06297996cf3e42cba1090f2405b5f3610900e189
Connor James Smith <cjs.connor.smith@gmail.com>
2023-07-23 13:43:22 -0400

    add beta9 define statements

--- src/main/scala/net/tazato/surreal/ql/statements/Define.scala
+++ src/main/scala/net/tazato/surreal/ql/statements/Define.scala
@@ -6,50 +6,37 @@ import net.tazato.surreal.ql.types.*
 sealed trait Define
 
 object Define extends Render[Define]:
+  case class NAMESPACE(name: String) extends Define:
+    lazy val render = Define.render(this)
 
-  case class NAMESPACE(name: String) extends Define with Render[NAMESPACE]:
-    override def render(x: NAMESPACE): String = s"NAMESPACE ${x.name}"
-  end NAMESPACE
+  case class DATABASE(name: String) extends Define:
+    lazy val render = Define.render(this)
 
-  case class DATABASE(name: String) extends Define with Render[DATABASE]:
-    override def render(x: DATABASE): String = s"DATABASE ${x.name}"
-  end DATABASE
+  case class FUNCTION(name: String, args: Seq[(String, String)], block: String) extends Define:
+    lazy val render = Define.render(this)
 
   case class LOGIN(
       name: String,
       base: Base,
       hash: String
-  ) extends Define
-      with Render[LOGIN]:
-    override def render(x: LOGIN): String = s"LOGIN ${x.name} ON ${x.base.value} PASSHASH ${x.hash}"
-  end LOGIN
+  ) extends Define:
+    lazy val render = Define.render(this)
 
   case class TOKEN(
       name: String,
       base: Base,
       kind: Algo,
       code: String
-  ) extends Define
-      with Render[TOKEN]:
-    override def render(x: TOKEN): String =
-      s"TOKEN ${x.name} ON ${x.base.value} TYPE ${x.kind.value} VALUE ${x.code}"
-  end TOKEN
+  ) extends Define:
+    lazy val render = Define.render(this)
 
   case class SCOPE(
       name: String,
       session: Option[String] = None,
       signup: Option[String] = None,
       signin: Option[String] = None
-  ) extends Define
-      with Render[SCOPE]:
-    override def render(x: SCOPE): String =
-      List(
-        s"SCOPE ${x.name}".some,
-        x.session.map(y => s"SESSION $y"),
-        x.signup.map(y => s"SIGNUP $y"),
-        x.signin.map(y => s"SIGNIN $y")
-      ).flatten.mkString(" ")
-  end SCOPE
+  ) extends Define:
+    lazy val render = Define.render(this)
 
   case class TABLE(
       name: String,
@@ -57,34 +44,16 @@ object Define extends Render[Define]:
       full: Boolean = false,
       view: Option[String] = None,
       permissions: Option[String] = None // TODO permissions needs to be a sub-statement
-  ) extends Define
-      with Render[TABLE]:
-    override def render(x: TABLE): String =
-      val d: Option[String] = if (x.drop) Some("DROP") else None
-      val f: String         = if (x.full) "SCHEMAFULL" else "SCHEMALESS"
-      val p: String = x.permissions match
-        case Some(y) => y
-        case None    => "PERMISSIONS FULL" // default to full access... maybe bad?
-
-      List(
-        s"TABLE ${x.name}".some,
-        d,
-        f.some,
-        x.view,
-        p.some
-      ).flatten.mkString(" ")
-  end TABLE
+  ) extends Define:
+    lazy val render = Define.render(this)
 
   case class EVENT(
       name: String,
       what: String,
       when: String,
       `then`: String
-  ) extends Define
-      with Render[EVENT]:
-    override def render(x: EVENT): String =
-      s"EVENT ${x.name} ON ${x.what} WHEN ${x.when} THEN ${x.`then`}"
-  end EVENT
+  ) extends Define:
+    lazy val render = Define.render(this)
 
   case class FIELD(
       name: String,
@@ -93,46 +62,65 @@ object Define extends Render[Define]:
       value: Option[String] = None,
       assert: Option[String] = None,
       permissions: Option[String] = None // TODO permissions needs to be a sub statement
-  ) extends Define
-      with Render[FIELD]:
-    override def render(x: FIELD): String =
-      val p: String = x.permissions match
-        case Some(y) => y
-        case None    => "PERMISSIONS FULL"
-
-      List[Option[String]](
-        s"FIELD $name ON $what".some,
-        x.kind.map(y => s"TYPE $y"),
-        x.value.map(y => s"VALUE $y"),
-        x.assert.map(y => s"ASSERT $y"),
-        p.some
-      ).flatten.mkString(" ")
-  end FIELD
+  ) extends Define:
+    lazy val render = Define.render(this)
 
   case class INDEX(
       name: String,
       what: String,
       cols: String,
       uniq: Boolean
-  ) extends Define
-      with Render[INDEX]:
-    override def render(x: INDEX): String =
-      val u: Option[String] = if (x.uniq) Some("UNIQUE") else None
-      List[Option[String]](
-        s"INDEX ${x.name} ON ${x.what} FIELDS ${x.cols}".some,
-        u
-      ).flatten.mkString(" ")
-  end INDEX
+  ) extends Define:
+    lazy val render = Define.render(this)
 
-  override def render(x: Define): String =
-    def define(s: String): String = s"DEFINE $s;"
-    x match
-      case y: NAMESPACE => define(y.render(y))
-      case y: DATABASE  => define(y.render(y))
-      case y: LOGIN     => define(y.render(y))
-      case y: TOKEN     => define(y.render(y))
-      case y: SCOPE     => define(y.render(y))
-      case y: TABLE     => define(y.render(y))
-      case y: EVENT     => define(y.render(y))
-      case y: FIELD     => define(y.render(y))
-      case y: INDEX     => define(y.render(y))
+  override def render(x: Define): String = {
+    val last = x match {
+      case NAMESPACE(name) => s"NAMESPACE $name"
+      case DATABASE(name)  => s"DATABASE $name"
+      case FUNCTION(name, args, block) => {
+        val argsString = args
+          .map { case (n, k) =>
+            s"$n: $k"
+          }
+          .mkString(", ")
+        s"FUNCTION fn::$name($argsString) $block"
+      }
+      case LOGIN(n, b, h)    => s"LOGIN $n ON ${b.value} PASSHASH $h"
+      case TOKEN(n, b, k, c) => s"TOKEN $n ON ${b.value} TYPE ${k.value} VALUE $c"
+      case SCOPE(n, session, signup, signin) =>
+        List(
+          s"SCOPE $n".some,
+          session.map(y => s"SESSION $y"),
+          signup.map(y => s"SIGNUP $y"),
+          signin.map(y => s"SIGNIN $y")
+        ).flatten.mkString(" ")
+      case TABLE(n, drop, full, view, permissions) =>
+        val d: Option[String] = if (drop) Some("DROP") else None
+        val f: String         = if (full) "SCHEMAFULL" else "SCHEMALESS"
+        val p: String         = permissions.getOrElse("PERMISSIONS FULL")
+        List(
+          s"TABLE $n".some,
+          d,
+          f.some,
+          view,
+          p.some
+        ).flatten.mkString(" ")
+      case EVENT(n, what, when, t) => s"EVENT $n ON $what WHEN $when THEN $t"
+      case FIELD(name, what, kind, value, assert, permissions) =>
+        val p: String = permissions.getOrElse("PERMISSIONS FULL")
+        List[Option[String]](
+          s"FIELD $name ON $what".some,
+          kind.map(y => s"TYPE $y"),
+          value.map(y => s"VALUE $y"),
+          assert.map(y => s"ASSERT $y"),
+          p.some
+        ).flatten.mkString(" ")
+      case INDEX(name, what, cols, uniq) =>
+        val u: Option[String] = if (uniq) Some("UNIQUE") else None
+        List[Option[String]](
+          s"INDEX $name ON $what FIELDS $cols".some,
+          u
+        ).flatten.mkString(" ")
+    }
+    s"DEFINE $last;"
+  }
--- src/test/scala/net/tazato/surreal/ql/statements/DefineSuite.scala
+++ src/test/scala/net/tazato/surreal/ql/statements/DefineSuite.scala
@@ -5,69 +5,85 @@ import net.tazato.surreal.ql.types.*
 import weaver._
 
 object DefineSuite extends FunSuite:
-  import Define._
   test("DEFINE NAMESPACE works") {
     val expected = "DEFINE NAMESPACE tazato;"
-    val rendered = Define.render(NAMESPACE("tazato"))
+    val rendered = Define.NAMESPACE("tazato").render
     expect.same(expected, rendered)
   }
 
   test("DEFINE DATABASE works") {
     val expected = "DEFINE DATABASE tazato;"
-    val rendered = Define.render(DATABASE("tazato"))
+    val rendered = Define.DATABASE("tazato").render
     expect.same(expected, rendered)
+  }
+
+  test("DEFINE FUNCTION works") {
+    val expected =
+      """DEFINE FUNCTION fn::greet($name: string) {
+	      |  RETURN "Hello, " + $name + "!";
+        |};""".stripMargin
+    val rendered = Define
+      .FUNCTION(
+        "greet",
+        Seq(("$name", "string")),
+        """{
+        |  RETURN "Hello, " + $name + "!";
+        |}""".stripMargin
+      )
+      .render
+    assert(expected == rendered)
   }
 
   test("DEFINE SCOPE works") {
     val expected = "DEFINE SCOPE taz SESSION sexy SIGNUP taz;"
-    val rendered = Define.render(SCOPE("taz", Some("sexy"), Some("taz"), None))
+    val rendered = Define.SCOPE("taz", Some("sexy"), Some("taz"), None).render
     expect.same(expected, rendered)
   }
 
   test("DEFINE LOGIN works") {
     val expected = "DEFINE LOGIN taz ON NAMESPACE PASSHASH fakehashrofl;"
-    val rendered = Define.render(LOGIN("taz", Base.NAMESPACE, "fakehashrofl"))
+    val rendered = Define.LOGIN("taz", Base.NAMESPACE, "fakehashrofl").render
     expect.same(expected, rendered)
   }
 
   test("DEFINE TOKEN works") {
     val expected = "DEFINE TOKEN tazato ON NS TYPE RS256 VALUE fakevaluerofl;"
-    val rendered = Define.render(TOKEN("tazato", Base.NS, Algo.Rs256, "fakevaluerofl"))
+    val rendered = Define.TOKEN("tazato", Base.NS, Algo.Rs256, "fakevaluerofl").render
     expect.same(expected, rendered)
   }
 
   test("DEFINE SCOPE works, just a name") {
     val expected = "DEFINE SCOPE taz;"
-    val rendered = Define.render(SCOPE("taz"))
+    val rendered = Define.SCOPE("taz").render
     expect.same(expected, rendered)
   }
 
   test("DEFINE SCOPE works, a couple optionals") {
     val expected = "DEFINE SCOPE taz SIGNIN taz;"
-    val rendered = Define.render(SCOPE("taz", signin = Some("taz")))
+    val rendered = Define.SCOPE("taz", signin = Some("taz")).render
     expect.same(expected, rendered)
   }
 
   test("DEFINE TABLE with simple name, no schema works TODO fix permissions") {
     val expected = "DEFINE TABLE taz SCHEMALESS PERMISSIONS FULL;"
-    val rendered = Define.render(TABLE("taz"))
+    val rendered = Define.TABLE("taz").render
     expect.same(expected, rendered)
   }
 
   test("DEFINE EVENT works") {
     val expected = "DEFINE EVENT taz ON kitchen WHEN needed THEN dinner;"
-    val rendered = Define.render(EVENT("taz", "kitchen", "needed", "dinner"))
+    val rendered = Define.EVENT("taz", "kitchen", "needed", "dinner").render
     expect.same(expected, rendered)
   }
 
   test("DEFINE FIELD works TODO fix permissions") {
     val expected = "DEFINE FIELD taz ON kitchen TYPE float VALUE 0.0 PERMISSIONS FULL;"
-    val rendered = Define.render(FIELD("taz", "kitchen", kind = Some("float"), value = Some("0.0")))
+    val rendered = Define.FIELD("taz", "kitchen", kind = Some("float"), value = Some("0.0")).render
     expect.same(expected, rendered)
   }
 
   test("DEFINE INDEX works") {
     val expected = "DEFINE INDEX taz ON kitchen FIELDS fork, knife UNIQUE;"
-    val rendered = Define.render(INDEX("taz", "kitchen", cols = "fork, knife", uniq = true))
+    val rendered = Define.INDEX("taz", "kitchen", cols = "fork, knife", uniq = true).render
     expect.same(expected, rendered)
   }
